8a10dd307328a0d2c296911ea08c4ce804b3edcf
Roman Kholopov <roma.kholopov@gmail.com>
2022-05-18 20:50:10 +0300

    rebild code

--- routes/test.js
+++ routes/test.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // res.send('test done!');
+  res.render('Test', { condition: 'done!' });
 });
 
 module.exports = router;
