10d9b1bbdbfd2e04d35c905b206136b906419527
Roman-Kholopov <87388143+Roman-Kholopov@users.noreply.github.com>
2022-05-31 20:34:39 +0300

    fix: App goes to crash in /users page (#3)
    * fix: App goes to crash in /users page
    
    * changes: delete unnecessary comments
    
    * change: throw new error => err

--- controllers/user.js
+++ controllers/user.js
@@ -1,17 +1,22 @@
 const User = require('../models/User');
 
-async function getUserList(req, res) {
-    const user = new User();
+async function getUserList(req, res, next) {
+    try {
+        const user = new User();
 
-    const {data} = await user.fetchUsers();
-
-    const userList = data.map((user) => ({
-        username: user.username,
-        email: user.email,
-    }));
-
-    res.render('user/list', { users: userList });
+        const {data} = await user.fetchUsers();
+    
+        const userList = data.map((user) => ({
+            username: user.username,
+            email: user.email,
+        }));
+    
+        res.render('user/list', { users: userList });    
+    } catch (err) {
+        next(err);
+    }
 
 }
+
 
 module.exports.getUserList = getUserList;
--- models/User.js
+++ models/User.js
@@ -5,6 +5,7 @@ class User {
   async fetchUsers() {
     try {
       const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+
       return response;
     } catch (error) {
       console.error(error);
