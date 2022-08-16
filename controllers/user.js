c314325a9d1a2fa0dd218c09a32538d8e9a4c79b
Roman Kholopov <roma.kholopov@gmail.com>
2022-08-16 15:08:40 +0300

    feat: create show button

--- controllers/user.js
+++ controllers/user.js
@@ -7,6 +7,7 @@ async function getUserList(req, res, next) {
         const {data} = await user.fetchUsers();
     
         const userList = data.map((user) => ({
+            id: user.id,
             username: user.username,
             email: user.email,
         }));
@@ -18,5 +19,22 @@ async function getUserList(req, res, next) {
 
 }
 
+async function getUserById(req, res, next) {
+    try {
+        const user = new User();
+
+        const userId = req.params.userId;
+
+        const carrentUser = await user.getUserById(userId);
+
+        console.log(carrentUser);
+
+        res.render('user/user', { user: carrentUser});
+    } catch (err) {
+        next(err);
+    }
+
+}
 
 module.exports.getUserList = getUserList;
+module.exports.getUserById = getUserById;
