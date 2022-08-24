8af6949357c631f6b7ab753aaeb266c3277f633b
Roman Kholopov <roma.kholopov@gmail.com>
2022-08-24 11:37:45 +0300

    feat: write logic for button delete and write logic for New user

--- models/User.js
+++ models/User.js
@@ -22,14 +22,36 @@ class User {
     }
   }
 
-  deleteUserById() {
-  //TODO: реализация будет позже
+  async deleteUserById(id) {
+    try {
+      const response = axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`);
+
+      return response;
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   updateUserById() {
   //TODO: реализация будет позже
   }
 
+  async createUser(body) {
+    try {
+      const {username, email, street, city, phone} = body;
+    const { data } = await axios.post('https://jsonplaceholder.typicode.com/users', {
+      username: username,
+      email: email,
+      street: street,
+      city: city,
+      phone: phone
+    })
+
+    return data;
+    } catch (err) {
+      console.error(err);
+    }
+  }
 }
 
 module.exports = User;
